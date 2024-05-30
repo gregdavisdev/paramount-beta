a53eaaf84d697b49930f675205c4fe95941387df
Gregory Davis <greg@davisdev.io>
2024-05-30 15:36:54 -0500

    Base routes and sign-in layout added

---
+++ src/lib/components/AnimatedRoute.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { page } from "$app/stores";
+  import { fly } from "svelte/transition";
+
+</script>
+
+{#key $page.url}
+  <div in:fly={{ x:'-100%', duration:500}}>
+    <slot />
+  </div>
+{/key}
--- src/routes/login/+layout.svelte
+++ src/routes/login/+layout.svelte
@@ -1,5 +1,31 @@
 <script lang="ts">
-    import type { LayoutData } from './$types';
-    
-    export let data: LayoutData;
+    import { page } from "$app/stores";
+    import AnimatedRoute from "$lib/components/AnimatedRoute.svelte";
+
 </script>
+
+<nav class="flex justify-center my-6">
+    <ul class="steps">
+        <a  href="/login" class="step step-primary">Login</a>
+        <a 
+            href="/login/username"
+            class="step"
+            class:step-primary={$page.route.id?.match(/username|photo/g)}>
+            Username
+        </a>
+        <a 
+            href="/login/photo"
+            class="step"
+            class:step-primary={$page.route.id?.match(/photo/g)}>
+            Photo
+        </a>
+    </ul>
+</nav>
+
+<AnimatedRoute>
+    <main class="card w-4/6 bg-neutral text-neutral-content mx-auto">
+        <div class="card-body items-center text-center">
+            <slot></slot>
+        </div>
+    </main>
+</AnimatedRoute>
