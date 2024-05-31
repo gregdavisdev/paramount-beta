67c5dc13fff9a5a8a6413daf9e58e99ecc67def9
Gregory Davis <greg@davisdev.io>
2024-05-31 15:21:21 -0500

    Google sign in added

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
-    import "../app.css";
-  </script>
+  import "../app.css";
+</script>
   
+<div class="min-h-screen flex flex-col">
   <slot />
+</div>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,5 +1,14 @@
 <script lang="ts">
+  import { auth } from "$lib/firebase";
+  import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 
+  async function signInWithGoogle() {
+    const provider = new GoogleAuthProvider();
+    const user = await signInWithPopup(auth, provider);
+    console.log(user)
+  }
 </script>
 
 <h2>Login</h2>
+
+<button class="btn btn-primary" on:click={signInWithGoogle}>Sign in with Google</button>
