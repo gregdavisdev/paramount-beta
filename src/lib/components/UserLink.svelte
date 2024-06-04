a06f4d75ffce3dcd8e683f2279707ffccc701e1f
Gregory Davis <greg@davisdev.io>
2024-06-04 16:18:34 -0500

    Added dynamic user data to page loads

---
+++ src/lib/components/UserLink.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let icon = 'default';
+  export let url = 'default url';
+  export let title = 'default title';
+</script>
+
+<a href={url} class="stack w-full max-w-md text-center bg-base-300 flex justify-center items-center p-4 rounded-lg not-prose no-underline">
+  <img src={`${icon}.png`} alt={icon} width="32" height="32" class="w-8" />
+  <span class="text-lg text-white font-bold">{title}</span>
+</a>
+
---
+++ src/routes/[username]/+page.ts
@@ -0,0 +1,32 @@
+import type { PageLoad } from './$types';
+import { collection, getDocs, limit, query, where } from 'firebase/firestore';
+import { db } from '$lib/firebase';
+import { error } from '@sveltejs/kit';
+
+export const load = (async ({params}) => {
+    const collectionRef = collection(db, "users");
+    const q = query(
+        collectionRef,
+        where("username", "==", params.username),
+        limit(1)
+    );
+
+    const snapshot = await getDocs(q);
+    const exists = snapshot.docs[0]?.exists();
+    const data = snapshot.docs[0]?.data();
+
+    if (!exists) {
+        throw error(404, "That user does not exist!");
+    }
+
+    if (!data.published) {
+        throw error(403, `The profile of @${data.username} is not public!`);
+    }
+
+    return {
+        username: data.username,
+        photoURL: data.photoURL,
+        bio: data.bio,
+        links: data.links ?? [],
+    };
+}) satisfies PageLoad;
