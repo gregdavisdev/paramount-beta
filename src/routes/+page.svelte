4b9983bfd58fb4ef49f117debf3a0d917a5e9598
Jessica Davis <jessica@davisdev.io>
2024-06-03 21:42:49 -0500

    Created hero section and card

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,15 +3,34 @@
 
 </script>
 
-<AnimatedRoute>
-  <div class="hero min-h-screen bg-base-200">
-    <div class="hero-content flex-col lg:flex-row-reverse">
-      <img src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" alt="stock"class="max-w-sm rounded-lg shadow-2xl" />
+<div class="hero min-h-screen bg-base-200">
+    <div class="hero-content flex-col lg:flex-row-reverse">  
+
+      <div class="card w-96 bg-indigo-50 text-black">
+        <div class="card-body">
+          <p>Explore our array of services designed to meet your unique health needs, and embark on a journey towards a healthier, happier life.</p>
+          <h3 class="font-bold">20 years of Happy Patients</h3>
+        </div>
+      </div>
+      
       <div>
-        <h1 class="text-5xl font-bold">Box Office News!</h1>
-        <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-        <button class="btn btn-primary">Get Started</button>
+        <button class="btn bg-indigo-50 text-gray-400">
+            &#x274A; Learn More
+        </button>
+        <h1 class="text-6xl font-semibold font-sans">Fostering Health, Providing Attention to You</h1>
+        <button class="btn btn-wide btn-primary text-lg">Set a Meeting &#124; &rarr;</button>
       </div>
     </div>
   </div>
-</AnimatedRoute>
+
+  <div class="card w-96 bg-base-100 shadow-xl">
+    <figure class="px-10 pt-10">
+      <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" class="rounded-xl" />
+    </figure>
+    <div class="card-body items-center text-center">
+      <h2 class="card-title">Vascular Heart</h2>
+      <p class="text-gray-500">Our visual emphasis on heart wellness extends beyond the surface.</p>
+      <div class="card-actions">
+      </div>
+    </div>
+  </div>
